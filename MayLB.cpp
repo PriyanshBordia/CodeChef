ee36e74b665d7b3f03381995b7fba43b2b74a66c
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-02 23:32:07 +0530

    may luch time

--- MayLB.cpp
+++ MayLB.cpp
@@ -31,28 +31,25 @@ void solve()
 
 	for (int i = 0; i < k; i++)
 	{
-		if (k & 1)
+		if (s[q[i] - 1] == '0')
+			s[q[i] - 1] = '1';
+		else
+			s[q[i] - 1] = '0';
+
+		if ((q[i] - 2) >= 0)
 		{
-			if (s[q[i] - 1] == '0')
-				s[q[i] - 1] = '1';
+			if (s[q[i] - 1] == s[q[i] - 2])
+				ans += 1;
 			else
-				s[q[i] - 1] = '0';
+				ans -= 1;
+		}
 
-			if ((q[i] - 2) >= 0)
-			{
-				if (s[q[i] - 1] == s[q[i] - 2])
-					ans += 1;
-				else
-					ans -= 1;
-			}
-
-			if (q[i] < n)
-			{
-				if (s[q[i] - 1] == s[q[i]])
-					ans += 1;
-				else
-					ans -= 1;
-			}
+		if (q[i] < n)
+		{
+			if (s[q[i] - 1] == s[q[i]])
+				ans += 1;
+			else
+				ans -= 1;
 		}
 
 		cout << ans << endl;
