d28d4d673e7946e2d093234545fbb09d27d2b9d0
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-08 23:23:15 +0530

    jun long

--- JunLD.cpp
+++ JunLD.cpp
@@ -4,22 +4,37 @@
 #include <algorithm>
 #include <assert.h>
 #include <vector>
-#include <set>
-#include <stack>
-#include <queue>
-#define MOD 1e09 + 7
 
 typedef long long ll;
+const ll MOD = 1e09 + 7;
 
 using namespace std;
 
+int power(int a, int b)
+{
+	if (b == 0)
+		return 1;
+
+	if (b == 1)
+		return a;
+	
+	ll t = power(a, b / 2);
+	t = (t * t) % MOD; 
+	
+	if (b % 2)
+		return a * t % MOD;
+	
+	return t;
+}
+
 void solve()
 {
-	ll ans(0), sum(0), cnt(0), mx(-1e18), mn(1e18);
+	ll ans(1), sum(0), cnt(1), mx(-1e18), mn(1e18);
 
 	ll n, m; cin >> n >> m;
 
-	ans += pow(pow(2, m) - 1, (n - 1));
+	cnt = power(2, n) - 1;
+	ans = power(cnt, m);
 
 	cout << ans << endl;
 
